aabadcf935a366c4e58e213d253d379b2c848cfd
rsekhon2015 <rsekhon@madisoncollege.edu>
2015-12-13 17:20:27 -0600

    not trying to be fancy anymore basic project

---
+++ ConnectGAPI/src/example/ConnectGAPI.java
@@ -0,0 +1,108 @@
+package example;
+
+import javax.net.ssl.HttpsURLConnection;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * @author Ranjit Sekhon
+ * Group Project - Using GOOGLE PLACES IPA - ALTER LONGITUDE AND LATITIUE FOR NEW REPONSE IN POST METHOD
+ * 12/13/2015
+ */
+
+public class ConnectGAPI {
+
+    private final String USER_AGENT = "Mozilla/5.0";
+
+    public static void main(String[] args) throws Exception {
+
+        ConnectGAPI http = new ConnectGAPI();
+
+        System.out.println("Testing 1 - Send Http GET request");
+        http.sendGet();
+
+        System.out.println("\nTesting 2 - Send Http POST request");
+        http.sendPost();
+
+    }
+
+    // HTTP GET request
+    private void sendGet() throws Exception {
+
+        String url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&types=food&name=cruise&key=AIzaSyCoMDYtoiZegI871v33nMp0OklyYBAGFS4";
+
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+        // optional default is GET
+        con.setRequestMethod("GET");
+
+        //add request header
+        con.setRequestProperty("User-Agent", USER_AGENT);
+
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'GET' request to URL : " + url);
+        System.out.println("Response Code : " + responseCode);
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        //print result
+        System.out.println(response.toString());
+
+    }
+
+    // HTTP POST request
+    private void sendPost() throws Exception {
+
+        String url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&types=food&name=cruise&key=AIzaSyCoMDYtoiZegI871v33nMp0OklyYBAGFS4";
+        URL obj = new URL(url);
+        HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+
+        //add reuqest header
+        con.setRequestMethod("POST");
+        con.setRequestProperty("User-Agent", USER_AGENT);
+        con.setRequestProperty("Accept-Language", "text/json");
+
+        String urlParameters = "location=-20.8670522,150.1957362&radius=500&types=food&name=cruise&key=AIzaSyCoMDYtoiZegI871v33nMp0OklyYBAGFS4";
+        //String urlParameters = THE USER NAME CHANGE THE LOCATION LONG & LATITUDE IN THIS STRING URL
+        //This allows you to write to the URL string
+
+        // Send post request
+        con.setDoOutput(true);
+        DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+        wr.writeBytes(urlParameters);
+        wr.flush();
+        wr.close();
+
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'POST' request to URL : " + url);
+        System.out.println("Post parameters : " + urlParameters);
+        System.out.println("Response Code : " + responseCode);
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        in.close();
+
+        //print result
+        System.out.println(response.toString());
+
+    }
+
+}
